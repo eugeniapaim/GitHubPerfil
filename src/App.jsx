47de9f3886ab36495023284f4b7ee672dc36b9bd
Eugênia Paim <eugheniasp@gmail.com>
2026-04-02 20:23:25 -0300

    bg amarelo em toda tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import Perfil from "./components/Perfil";
-//import Formulario from "./components/Formulario";
 import { useState } from "react";
 import styles from "./components/ReposList/ReposList.module.css";
 import ReposList from "./components/ReposList";
 
 
+
 function App() {
   // const [FormularioVisivel, setFormularioVisivel] = useState(true)
   const [nomeUsuario, setNomeUsuario] = useState('')
-  
+
 
   return (
     <>
-      <h3 className={styles.tituloNomeUsuario}>Nome de usuário GitHub:</h3>
-      <input type="text" className={styles.inputNomeUsuario} onBlur={(e) => setNomeUsuario(e.target.value)} placeholder="@"/> 
+      <div className={styles.headerAll}>
+        <h3 className={styles.tituloNomeUsuario}>Nome de usuário GitHub:</h3>
+        <input type="text" className={styles.inputNomeUsuario} onBlur={(e) => setNomeUsuario(e.target.value)} placeholder="@" />
+      </div>
       {nomeUsuario.length > 3 && (
         <>
           <Perfil nomeUsuario={nomeUsuario} />
